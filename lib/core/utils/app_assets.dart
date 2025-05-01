324b664150db4005e24b37a842204b39ce1c0536
Mahmoud Azmy <144074729+Mahmoud-Azmy@users.noreply.github.com>
2025-05-01 06:28:45 +0300

    add onBoarding assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,12 @@
+class AppAssets {
+  static const String onBoarding = 'assets/imgs/boardingBkg.png';
+  static const String onB1 = 'assets/svgs/onB1.svg';
+  static const String onB2 = 'assets/svgs/onB2.svg';
+  static const String onB3 = 'assets/svgs/onB3.svg';
+  static const String onB4 = 'assets/svgs/onB4.svg';
+  static const String empty = 'assets/images/empty.png';
+  static const String emptyCart = 'assets/images/empty_cart.png';
+  static const String emptyWishlist = 'assets/images/empty_wishlist.png';
+  static const String emptyOrder = 'assets/images/empty_order.png';
+  static const String emptyAddress = 'assets/images/empty_address.png';
+}
--- lib/features/onBoarding/on_boardign.dart
+++ lib/features/onBoarding/on_boardign.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:vibe_link/core/utils/app_assets.dart';
 
 class OnBoardign extends StatelessWidget {
   const OnBoardign({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("OnBoarding")));
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppAssets.onBoarding),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'OnBoarding',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
   }
 }
