07ee776c01592548402df6a98238fb618e7327ac
Mahmoud Azmy <144074729+Mahmoud-Azmy@users.noreply.github.com>
2025-05-08 22:05:47 +0300

    added botton Nav Bar Icons to assets

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -9,6 +9,17 @@ class AppAssets {
   static const String facebook = 'assets/svgs/facebook_icon.svg';
   static const String hide = 'assets/svgs/Hide.svg';
   static const String show = 'assets/svgs/Show.svg';
+  static const String activeHome = 'assets/icons/active_home.svg';
+  static const String inactiveHome = 'assets/icons/Home.svg';
+  static const String activeProfile = 'assets/icons/active_profile.svg';
+  static const String inactiveProfile = 'assets/icons/Profile.svg';
+  static const String activeCategory = 'assets/icons/active_category.svg';
+  static const String inactiveCategory = 'assets/icons/Category.svg';
+  static const String activeNotification =
+      'assets/icons/active_notification.svg';
+  static const String inactiveNotification = 'assets/icons/Notification.svg';
+  static const String plus = 'assets/icons/Plus.svg';
+  static const String inactiveCart = 'assets/icons/Cart.svg';
   static const String emptyCart = 'assets/images/empty_cart.png';
   static const String emptyWishlist = 'assets/images/empty_wishlist.png';
   static const String emptyOrder = 'assets/images/empty_order.png';
